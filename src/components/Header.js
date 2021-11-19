4a2465ef1ba333b1c42f6796fdc6c4a24f57d974
AlexBrasoveanu <alexandru.brasoveanu7@gmail.com>
2021-11-19 14:38:38 +0200

    mobile menus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from "react-router-dom";
 
@@ -7,12 +7,30 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 
 function Header() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar (!sidebar);
+
     return (
         <div className="header">
             <Link to="/"><div className="header--logo">
                 <img src={Logo} alt="" />
                 <span>adstoria</span>
             </div></Link> 
+
+            <div className="header--nav-mobile">
+                        <AiOutlineMenu size="25px" onClick={showSidebar} />  
+                    </div>
+
+                    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>                        
+                        <div className="nav-menu-items">
+                            <Link to="/">Home</Link>
+                            <Link to="/services">Services</Link>
+                            <Link to="/portofolio">Portofolio</Link>
+                            <Link to="/about-us">About Us</Link>
+                            <Link to="/contact">Contact</Link>
+                        </div>                  
+                    </nav>
 
             <div className="header--nav">
                 <Link to="/">Home</Link>
