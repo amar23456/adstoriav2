c90480142a3be5d4ea8ad56e815e3fdccc5caa1c
AlexBrasoveanu <alexandru.brasoveanu7@gmail.com>
2021-12-16 14:41:52 +0200

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 
+import { RiPhoneFill, RiMailFill, RiMapPin2Fill, RiFacebookFill, RiInstagramFill, RiYoutubeFill } from 'react-icons/ri';
+
+import Fade from 'react-reveal/Fade';
+
 function Contact() {
     return (
         <>
             <Header />
-            
-            <div className="hero">
-                <h1>Contact</h1>
-            </div>
 
             <div className="main_contact">
                 <div className="main_contact--informations">
@@ -17,14 +17,24 @@ function Contact() {
                         <p>Feel free to ask us any questions</p>
                     </div>
                     <ul>
-                        <li>Phone Number</li>
-                        <li>Email</li>
-                        <li>Address</li>
+                        <li><RiPhoneFill size="30px" />+0123 4567 8910</li>
+                        <li><RiMailFill size="30px" />contact@adstoria.io</li>
+                        <li><RiMapPin2Fill size="30px" />102 Street 2714 Don</li>
                     </ul>
-                    <div className="main_contact--informations-icons">
-                        instagram
+                    <div className="main_contact--informations--icons">
+                        <div className="icon">
+                            <RiFacebookFill size="30px" />
+                        </div>
+                        <div className="icon">
+                             <RiInstagramFill size="30px" />   
+                        </div>
+                        <div className="icon">
+                            <RiYoutubeFill size="30px" />
+                        </div>
                     </div>
                 </div>
+
+
                 <div className="main_contact--form">
                     <form>
                         <h4>First Name</h4>
