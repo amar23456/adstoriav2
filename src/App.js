feff474f82fec99c217db39c89e68f21b9cfb69f
Alex Brasoveanu <alexandru.brasoveanu7@gmail.com>
2022-01-12 00:15:11 +0200

    fixing icons mobile

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="services" element={<Services />} />
         <Route path="portofolio" element={<Portofolio />} />
         <Route path="about-us" element={<About />} />
